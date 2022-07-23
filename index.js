77f3ec1af95af1b010a808ceb0a440450196c2c2
Ewerton Vinicius <ewertonvinicius@outlook.com.br>
2022-07-22 23:49:01 -0300

    Configuração de arquivos estaticos e template engine do projeto( EJS)

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
-
-
 const app = express();
 
-
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.get('/', (req , res) => {
-    res.send("Hello doidoooo");
+    res.render('home');
 });
 
 
 app.listen(3000, () => {
-    console.log('Rondando na porta 3000')
-})
+    console.log('Rondando na porta 3000');
+});
